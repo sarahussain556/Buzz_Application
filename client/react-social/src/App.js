f02e4fc5239895fd0e7ea2ff57da9477a081618b
Prakashttn <prakash.chand@tothenew.com>
2022-04-08 12:42:43 +0530

    Added routes in Buzz App

--- client/react-social/src/App.js
+++ client/react-social/src/App.js
@@ -2,10 +2,28 @@ import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Profile from "./pages/profile/Profile";
 import Register from "./pages/register/Register";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Redirect,
+  BrowserRouter,
+} from "react-router-dom";
+// import { useContext } from "react";
+// import { AuthContext } from "./context/AuthContext";
 
 function App() {
-  return <Profile />
-
+  // const { user } = useContext(AuthContext);
+  return (
+    <BrowserRouter>
+     <Routes>
+        <Route exact path="/" element={<Home />}/>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/profile/username" element={<Profile />} />
+     </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
